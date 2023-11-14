c3eb96ed467f763e6bdae0a3a4f7d4442daf51b6
Fisken2010 <135128174+Fisken2010@users.noreply.github.com>
2023-11-14 08:55:04 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
